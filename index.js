f76bf77ca0f3eaa4a324417798ba78c97537e2dc
Tiago S. Oliveira <TiagoProff@gmail.com>
2019-10-31 19:20:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const electron = require('electron');
 //import electron from 'electron'; // NodeJs not support
 
-const {app} = electron;
+const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
-    console.log('====================================');
-    console.log('A aplicacao foi criada.');
-    console.log('====================================');
+    //console.log('A aplicacao foi criada.');
+    const windowMenu = new BrowserWindow({});
+    windowMenu.loadURL(`file://${__dirname}/wmanager.html`);
 })
