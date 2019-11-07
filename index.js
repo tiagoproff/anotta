8e49d712b1183c63b5e288beb6639613976f7117
Tiago S. Oliveira <TiagoProff@gmail.com>
2019-11-07 08:21:31 -0300

    add window note

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const electron = require('electron');
 //import electron from 'electron'; // NodeJs not support
+const loki = require('lokijs');
 
-const {app, BrowserWindow} = electron;
+const { app, dialog, BrowserWindow, ipcMain } = electron;
+//const notesData = require ('./notes.json');
+
 
 app.on('ready', () => {
+    
+    
+
     //console.log('A aplicacao foi criada.');
-    const windowMenu = new BrowserWindow({
+    const windowManager = new BrowserWindow({
+        //frame: false,
         webPreferences: {
             nodeIntegration : true
         }
     });
 
-    windowMenu.loadURL(`file://${__dirname}/wmanager.html`);
-})
+    windowManager.loadURL(`file://${__dirname}/wmanager.html`);
+
+    ipcMain.on('newNote', (note) => {
+        //dialog.showOpenDialog();
+    
+        const windowNote = new BrowserWindow({
+            parent: windowManager,
+            //frame: false,
+            width: 340, 
+            height: 180,
+            maximizable: false,
+            backgroundColor: '#d7e632',
+            webPreferences: {
+                nodeIntegration : true
+            }
+        });
+
+        windowNote.loadURL(`file://${__dirname}/wnote.html`);
+
+        //windowNote.setBackgroundColor('#d7e632')
+        console.log(note);
+    });
+});
 
 
 
---
+++ wnote.js
@@ -0,0 +1,3 @@
+$(document).ready(() => {
+    $('body').html(note);
+});
